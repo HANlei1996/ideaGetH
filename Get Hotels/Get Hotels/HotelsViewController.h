de7e22b45dd9dda4d38dba00a6c866afbcac03a5
韩雷 <1132646503@qq.com>
2017-08-22 12:38:45 +0800

    总框架

---
+++ Get Hotels/Get Hotels/HotelsViewController.h
@@ -0,0 +1,13 @@
+//
+//  HotelsViewController.h
+//  Get Hotels
+//
+//  Created by admin on 2017/8/21.
+//  Copyright © 2017年 com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HotelsViewController : UIViewController
+
+@end
---
+++ Get Hotels/Get Hotels/MyViewController.h
@@ -0,0 +1,13 @@
+//
+//  MyViewController.h
+//  Get Hotels
+//
+//  Created by admin on 2017/8/21.
+//  Copyright © 2017年 com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyViewController : UIViewController
+
+@end
