b68a65f7aa73ff25041068c300e2f2844d6b82df
陈永剑 <1776968161@qq.com>
2017-08-23 18:43:16 +0800

    QQQ

--- Get Hotels/Get Hotels/MyHotelsViewController.m
+++ Get Hotels/Get Hotels/MyHotelsViewController.m
@@ -23,6 +23,25 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+//设置导航栏样式
+- (void)setNavigationItem{
+    //self.navigationController.navigationBar.backgroundColor = [UIColor whiteColor];
+     
+    //实例化一个button 类型为UIButtonTypeSystem
+    UIButton *leftBtn = [UIButton buttonWithType:UIButtonTypeSystem];
+    //设置位置大小
+    leftBtn.frame = CGRectMake(0, 0, 20, 20);
+    //设置其背景图片为返回图片
+    [leftBtn setBackgroundImage:[UIImage imageNamed:@"返回"] forState:UIControlStateNormal];
+    //给按钮添加事件
+    [leftBtn addTarget:self action:@selector(leftButtonAction:) forControlEvents:UIControlEventTouchUpInside];
+    
+    self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView:leftBtn];
+}
+//自定的返回按钮的事件
+- (void)leftButtonAction: (UIButton *)sender{
+    [self.navigationController popViewControllerAnimated:YES];
+}
 
 /*
 #pragma mark - Navigation
