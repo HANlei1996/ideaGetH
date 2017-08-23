91935f6149901bf6ce6ecafc4295b4b76bf8fb62
何伦 <938788163@qq.com>
2017-08-23 17:51:10 +0800

    拉线

---
+++ Get Hotels/Get Hotels/HotelsTableViewCell.h
@@ -0,0 +1,16 @@
+//
+//  HotelsTableViewCell.h
+//  Get Hotels
+//
+//  Created by admin on 2017/8/23.
+//  Copyright © 2017年 com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HotelsTableViewCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UIImageView *hotelsImage;
+@property (weak, nonatomic) IBOutlet UILabel *hotelsName;
+@property (weak, nonatomic) IBOutlet UILabel *hotelsmoney;
+@property (weak, nonatomic) IBOutlet UILabel *hotelsAddr;
+@property (weak, nonatomic) IBOutlet UILabel *hotelsDistance;@end
